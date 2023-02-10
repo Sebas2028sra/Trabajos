a7eaad8861cff0d84b134721a06753a54f7105db
Sebastián Rojas Araya <sebastian20.sra@gmail.com>
2023-02-09 20:56:33 -0600

    Primera clase con python

---
+++ clase1.py
@@ -0,0 +1,116 @@
+#ctrl + k + c para comentar el codigo y ctrl + k + u para descomentar el codigo
+
+# last_name = "Rojas"+"Araya"
+# name = "Sebastian"
+# print(name+""+last_name)
+
+# diferentes definiciones de variables 
+# name = "sebas"
+# id = 202931
+# money = 0.00
+# true_on = True
+
+
+# print("Load Data")
+# status = False
+# print(status)
+
+
+# sub = True
+# print(sub)
+
+
+# Ejemplo de un interruptor
+
+
+# is_day = False
+
+# lights_on = not is_day
+
+# print("Day time?")
+# print(is_day)
+
+# print("lights on")
+# print(lights_on) 
+
+
+#codigo de ventas y stock
+
+# stock = 600
+# jeans_sold = 500
+# target = 500
+
+# target_hit = jeans_sold == target
+# print("hit jeans sale target")
+# print(target_hit)
+
+# current_stock = stock - jeans_sold
+# in_stock = current_stock != 0
+
+# print("jeans in stock")
+# print(in_stock)
+
+#comparacion de numeros
+
+# print(1<9)
+
+# result = 15 <= 7
+# print(result)
+
+
+# battery_level = 30
+# low = battery_level <= 15
+
+# print(low)
+
+
+#comparacion de cadenas de texto
+
+# print("online" == "online")
+# print("online" != "ofline" )
+
+# medir frecuencia cardiaca
+
+# heart_rate = 77
+
+# low = heart_rate < 60
+# high = heart_rate > 100
+
+# print("Heart rate low")
+# Print(low)
+
+# print("Heart rate high")
+# print(high)
+
+
+# cadenas de formato
+
+new_message = 5
+print(f"{new_message} new messages ")
+
+
+new = 5
+read = 2
+
+print(f" {new - read } unread messages")
+
+print(f" {5} new message and {2} friends request") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
