a575e24e9311f0e2a0d7634701b7dd73993cdf2f
Sebastian Rojas <sebastian20.sra@gmail.com>
2023-02-16 21:11:13 -0600

    Segunda clase

---
+++ clase2.py
@@ -0,0 +1,145 @@
+# keep_going = True
+
+# while keep_going == True:
+#     print("otra vez")
+
+
+#controlar un bucle
+
+# counter = 1
+
+# while counter < 4:
+#     print(counter)
+#     counter += 1 
+
+
+
+
+# inter_shipping = True
+
+# total = 150
+# shipping_cost = 0
+
+# if inter_shipping:
+#     shipping_cost +=15
+#     print("international base cost applied")
+
+
+# if total <= 50:
+#     shipping_cost += 20
+# elif total <= 100:
+#     shipping_cost += 15
+# else:
+#     shipping_cost += 5
+
+# print(f"Spipping cost: {shipping_cost}")
+
+# account = 100
+# intereses = 0.10
+# years = 3
+
+# print(f"Initial amount:{account}")
+# counter = 1
+# while counter <= years:
+#     accrued_interest = account * intereses
+#     account += accrued_interest
+#     account += 100
+#     print(f"Year {counter}: {account}")
+#     counter +=1
+
+
+
+
+# #Creacion y usos de listas o array
+
+# age = [25,21,26,29,23,32]
+# age[2] = 40
+# print(age[2])
+
+# todo = ["read", "Workout", "code"]
+# print(todo)
+
+#agregar
+
+# scores = [24,23]
+# scores.append(25)
+
+# print(scores)
+
+# #insertar
+
+# shopping = ["kiwis", "peras", "tomates"]
+# shopping.insert(0,"limon")
+# print(shopping)
+
+# #eliminar
+
+# todo = ["read", "Workout", "code"]
+# todo.pop(1)
+# removed = todo.pop(1)
+# print(todo)
+# print(removed)
+
+#recorree array
+
+# final_scores = [17,22,34,13]
+
+# for score in final_scores:
+#     print(score)
+#     print("----------")
+
+
+# data_points = [99,99,99,99]
+# for data in data_points:
+#     print(data + 1)
+
+
+# users = ["juan","ana","pepe"]
+
+# number_of_users = len(users)
+
+# if number_of_users >0 :
+#     print(f"Online users: {number_of_users}")
+# else:
+#     print("Offline app")
+
+
+# flavors = ["cinnamon", "apple pie", "acid"]
+# print("New flavors")
+# print(flavors)
+
+# ratings = [4,2.5,3]
+# print("Consumer ratings")
+# print(ratings)
+
+
+
+
+#encontar minimos y maximos y ordenar datos
+
+
+# humedad = [87,83,107,88,66]
+# min= min(humedad)
+# max= max(humedad)
+# sorted = sorted(humedad)
+# sum = sum(humedad)
+# print(max)
+# print(min)
+# print(sorted)
+# print(sum)
+
+# #unir datos
+
+# dataset1 = [1,2,3]
+# dataset2 = [4,5]
+# combined = dataset1 + dataset2
+
+# print(combined)
+
+# humedad = [87,83,107,88,66,87,87,87,170]
+# count = humedad.count(87)
+# print(count)
+# print(87 in humedad)
+
+
+
