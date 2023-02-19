17a7c187607a3e16eb8cbe62ddb0228fe46cca78
Sebastian Rojas <sebastian20.sra@gmail.com>
2023-02-19 17:49:25 -0600

    calse1 guardada

--- clase1.py
+++ clase1.py
@@ -124,22 +124,22 @@
 #     print("Good afternoon ")
 
 
-ride_type = "Black"
-credits = 4
+# ride_type = "Black"
+# credits = 4
 
-ride_price = 0
-final_price = 0
+# ride_price = 0
+# final_price = 0
 
-if ride_type == "DooberX":
-    ride_price = 20.5
-elif ride_type == "Black":
-    ride_price = 37.9
-else:
-    ride_price = 18.7 
+# if ride_type == "DooberX":
+#     ride_price = 20.5
+# elif ride_type == "Black":
+#     ride_price = 37.9
+# else:
+#     ride_price = 18.7 
 
 
-print("Ride price")
-print(ride_price)
+# print("Ride price")
+# print(ride_price)
 
 
 
