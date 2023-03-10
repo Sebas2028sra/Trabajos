92e392066be40bc1be33d84bfdfb83ed855231df
Sebastian Rojas <sebastian20.sra@gmail.com>
2023-03-09 20:24:24 -0600

    ejemplos para avanzar con los trabajos

---
+++ ejemplo_listas.py
@@ -0,0 +1,22 @@
+#problema 1 (Definir una lista vacio y luego solicitar la entrada de 5 enteros y añadirlos a la lista y luego a imprimrlo)
+
+# lista = []
+
+# for x in range(5):
+#     valor = int(input("Ingrese un valor entero: "))
+#     lista.append(valor)
+
+
+# print(lista)
+
+# #problema 2
+
+# lista2 = []
+
+# valor2 = int(input("ingrese valor (0 para finalizar)"))
+
+# while valor2 != 0:
+#     lista2.append(valor2)
+#     valor2 = int(input("ingrese valor (0 para finalizar)"))
+
+# print("Tamaño de la lista: ",len(lista2))
---
+++ ejemplos_tuplas.py
@@ -0,0 +1,31 @@
+#una funcion que almacene la carga del dia, mes, año y almacene dichos datos en una tupla
+
+
+# def cargar_fecha():
+#     dd = int(input("Ingrese el numero de dia: "))
+#     mm = int(input("Ingrese el numero de mes: "))
+#     aa = int(input("Ingrese el numero de año: "))
+#     return (dd,mm,aa)
+
+# def imprimir_fecha(fecha):
+#     print(fecha[0],fecha[1],fecha[2],sep="/")
+
+# fecha=cargar_fecha()
+# imprimir_fecha(fecha)
+
+#empaquetado y desempaquetado de tuplas
+
+# x=10
+# y=30
+# punto = (x,y)
+
+# print(punto)
+
+# #tenemos tres variables de las cuales vamos a desempaquetar
+
+# fecha = (25, "diciembre", 2023)
+
+# dd,mm,aa = fecha
+# print("dia: ",dd)
+# print("mes: ",mm)
+# print("año: ",aa)
